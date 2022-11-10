efefe1f80db0572651cfc81e3769a9ac8e926bf1
Hai M Nguyen <vnjp22@gmail.com>
2022-11-10 11:52:03 -0800

    Added .env and .env.production

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Traderchain Interface</p>
+        <p>NODE_ENV: {process.env.NODE_ENV}</p>
+        <p>REACT_APP_DOMAIN: {process.env.REACT_APP_DOMAIN}</p>
       </header>
     </div>
   );
