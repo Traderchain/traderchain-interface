8c0410292a238affb8751269bdf3b4f5e889847a
Hai M Nguyen <vnjp22@gmail.com>
2022-11-13 21:19:32 -0800

    Init main contracts

--- src/contracts/ERC1155/index.ts
+++ src/contracts/ERC1155/index.ts
@@ -1,7 +1,7 @@
 import Contract from 'contracts/contract';
 import abi from './abi.json';
 
-export default class ERC20 extends Contract {    
+export default class ERC1155 extends Contract {    
   
   constructor(address: string) {
     super(address, abi);
--- src/contracts/SystemVault/index.ts
+++ src/contracts/SystemVault/index.ts
@@ -1,7 +1,7 @@
 import Contract from 'contracts/contract';
 import abi from './abi.json';
 
-export default class ERC20 extends Contract {    
+export default class SystemVault extends Contract {    
   
   constructor(address: string) {
     super(address, abi);
