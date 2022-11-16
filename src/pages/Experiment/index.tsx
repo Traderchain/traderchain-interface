5cf032cbaf90dea50735f536ecbbecffd29d7a45
Hai M Nguyen <vnjp22@gmail.com>
2022-11-16 14:44:28 -0800

    Experiment: refine color

--- src/pages/Experiment/index.tsx
+++ src/pages/Experiment/index.tsx
@@ -1,6 +1,6 @@
 // import { useEffect } from 'react';
 import Button from "@mui/material/Button";
-import { VuiButton } from 'traderchain-ui';
+import { VuiButton, VuiTypography } from 'traderchain-ui';
 import { isDevelopmentEnv, isProductionEnv } from 'utils/env';
 import { Address } from 'utils/constants';
 
@@ -74,14 +74,16 @@ export default function Experiment() {
   // }, []);
 
   return (
-    <div>
-      <h2>Experiment</h2>
-      <p>NODE_ENV: {process.env.NODE_ENV}</p>
-      <p>REACT_APP_DOMAIN: {process.env.REACT_APP_DOMAIN}</p>
-      <p>isDevelopmentEnv: {isDevelopmentEnv() ? 'true' : 'false'}</p>
-      <p>isProductionEnv: {isProductionEnv() ? 'true' : 'false'}</p>
-      <p><Button variant="outlined">Button</Button></p>      
-      <p><VuiButton variant="outlined" color="light">VuiButton</VuiButton></p>      
+    <div id="experiment">      
+      <VuiTypography color="white" variant="h2">Experiment</VuiTypography>
+      <VuiTypography color="white">        
+        NODE_ENV: {process.env.NODE_ENV}<br/>
+        REACT_APP_DOMAIN: {process.env.REACT_APP_DOMAIN}<br/>
+        isDevelopmentEnv: {isDevelopmentEnv() ? 'true' : 'false'}<br/>
+        isProductionEnv: {isProductionEnv() ? 'true' : 'false'}<br/>
+        <Button variant="outlined">Button</Button><br/>      
+        <VuiButton variant="outlined" color="light">VuiButton</VuiButton><br/>      
+      </VuiTypography>
     </div>
   );
 }
