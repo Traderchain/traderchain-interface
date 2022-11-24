3257e7b77e4e01d8d067041303362150709db6cb
Hai M Nguyen <vnjp22@gmail.com>
2022-11-23 17:01:54 -0800

    Shorten hash in a ExplorerLink

--- src/components/ExplorerLink/index.tsx
+++ src/components/ExplorerLink/index.tsx
@@ -2,6 +2,8 @@ import { Link } from "@mui/material";
 import { VuiTypography } from 'traderchain-ui';
 
 export default function ExplorerLink({ type, hash }: any) {
+  const shortedHash = hash ? `${hash.substring(0,8)}......${hash.substring(hash.length-6)}` : '';
+  
   let url = "https://goerli.etherscan.io";
   if (type == 'txn')  url += "/tx/" + hash;
   else  url += "/address/" + hash;
@@ -15,7 +17,7 @@ export default function ExplorerLink({ type, hash }: any) {
       variant="a"            
       sx={{ fontSize: "90%" }}
     >
-      {hash || ''}
+      {shortedHash}
     </VuiTypography>
   );
 }
