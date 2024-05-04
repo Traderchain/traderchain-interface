f7a94502664434f5adc2bfc9a3cad896660dba18
Hai M Nguyen <vnjp22@gmail.com>
2024-05-04 16:40:02 -0700

    Added Vercel Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import { Provider } from "react-redux";
 import store from "./stores";
 import 'assets/css/index.css';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(  
   <Provider store={store}>
     <App />
+    <Analytics />
   </Provider>
 );
 
